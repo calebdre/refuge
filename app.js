166f73ec10692472846f5c989ef2c023f3712cfb
Joshua King <joshua@antechdevelopment.com>
2016-10-23 02:42:58 -0400

    Added typeform iframe

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-function moveSectionLeft (selector) {
+var curSec, questionaireIdex = 0;
 
+function moveSectionOutToLeft (selector, callback) {
+	$(selector)
+		.animate({
+					 "left": "-100vw",
+					 "opacity": "0"
+				 }, 1000, callback);
 }
-
-function moveToNextScreen () {
-
+function moveSectionInFromRight (selector, callback) {
+	$(selector)
+		.animate({
+					 "right": "0%"
+				 }, 1000, callback);
+}
+function moveToNextScreen (newSec) {
+	if (curSec) {
+		moveSectionOutToLeft(curSec, function () {
+			moveSectionInFromRight(curSec = newSec);
+		});
+	}
 }
 
 window.onload = function () {
+	var sections = $(".section");
 
+	$("#section-next-btn").click(function () {
+		moveSectionOutToLeft(sections[ questionaireIdex++ ]);
+	})
 };
