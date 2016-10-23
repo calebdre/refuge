678a3605790834ddc2d468e05f36958bda41ed0a
Joshua King <joshua@antechdevelopment.com>
2016-10-23 04:59:31 -0400

    Updated some things...

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ function split (topS, botS, callback) {
 }
 
 window.onload = function () {
-	$("#volunteerBtn").click(function () {
+	$("#volunteerBtn, .volunteer-row, .volunteer-row img, .volunteer-row h1, .volunteer-row p").click(function () {
 		split(".hello-section", ".types-section", function () {
 			var $volunteer = $("#volunteer-form");
 			$volunteer.css({ "top": 0 });
 			$volunteer.fadeIn();
 		});
 	});
-	$("#refugeeBtn").click(function () {
+	$("#refugeeBtn, .refugee-row, .refugee-row img, .refugee-row h1, .refugee-row p").click(function () {
 		split(".hello-section", ".types-section", function () {
 			var $refugee = $("#refugee-form");
 			$refugee.css({ "top": 0 });
